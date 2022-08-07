9fd551b97bd5cc431a381d54c756c2d16f18614b
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-08-07 15:48:50 +0600

    Last update = 07/08/2022

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // App Name / Topbar Title
+
       home: const MyHomePage(title: 'Hello Flutter Developer'),
     );
   }
